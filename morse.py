1e376dc0378e95757a46c694cb3500d7521c6d95
d-pruszk <d.pruszkowsk@student.uw.edu.pl>
2025-04-24 18:50:17 +0200

    Morse code encryption and decryption function

---
+++ morse.py
@@ -0,0 +1,85 @@
+dictionary = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "Ą": ".-.-",
+    "Ć": "—.—..",
+    "Ę": "..—..",
+    "Ł": ".—..—",
+    "Ń": "——.——",
+    "Ó": "———.",
+    "Ś": "...—...",
+    "Ż": "——..—.",
+    "Ź": "——..—",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    " ": "",
+}
+rev_dict = {v: k for k, v in dictionary.items()}
+
+def morse(string, cipher):
+    """Function to cipher or decipher a message from/into morse code. Put cipher=True to cipher into morse code."""
+    if cipher:
+        result = ""
+        for i in range(len(string) - 1):
+            result += dictionary[string[i]] + " "
+        result += dictionary[string[len(string) - 1]]
+        return (result)
+    else:
+        result = ""
+        i = 0
+        temp = ""
+        while i <= len(string) - 2:
+            if string[i] == string[i + 1] and string[i + 1] == " ":
+                result += rev_dict[temp]
+                i += 1
+                result += " "
+                temp = ""
+            elif string[i] == " ":
+                result += rev_dict[temp]
+                temp = ""
+            else:
+                temp += string[i]
+            i += 1
+        temp += string[i]
+        result += rev_dict[temp]
+        return (result)
+
+# print(morse("HELLO WORLD",cipher=True))
+# print(morse(".... . .-.. .-.. ---  .-- --- .-. .-.. -..",cipher=False))
+# print(morse("PRZYKŁADOWE ZDANIE",cipher=True))
+# print(morse(".--. .-. --.. -.-- -.- .—..— .- -.. --- .-- .  --.. -.. .- -. .. .",cipher=False))
